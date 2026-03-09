6536c3ada84c771991b4ba6d6f6b09350f0fa408
mouxtaris <149571892+mouxtaris@users.noreply.github.com>
2026-03-09 15:27:25 +0200

    Update CORS and gitignore for frontend integration

--- app/main.py
+++ app/main.py
@@ -14,10 +14,11 @@ app = FastAPI(title=settings.APP_NAME)
 
 Base.metadata.create_all(bind=engine)
 
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://127.0.0.1:5173",
+        "http://localhost:5173",
         "http://127.0.0.1:8000",
         "http://localhost:8000",
     ],
